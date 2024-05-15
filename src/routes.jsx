25ff869b977cc249b460cd4446d4a5dfeff32fff
Digambar Chalkapure <digambarchalkapure@gmail.com>
2024-05-15 17:53:27 +0530

    initial commit

---
+++ src/routes.jsx
@@ -0,0 +1,42 @@
+import { Home, Profile, SignIn, SignUp } from "@/pages";
+
+export const routes = [
+  {
+    name: "home",
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    name: "about",
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    name: "services",
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    name: "industries",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    name: "case studies",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    name: "contacts",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    name: "discuss",
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  
+];
+
+export default routes;
